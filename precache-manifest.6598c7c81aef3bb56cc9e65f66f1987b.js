0de5cda54451350176c82407220d6181082b14f3
Kharoh <Gaulu03@gmail.com>
2019-10-13 11:29:49 +0200

    Updates

---
+++ precache-manifest.6598c7c81aef3bb56cc9e65f66f1987b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e47f9b24bccfc703b4701ed498c086c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92de5436519d10d3c7a3",
+    "url": "/static/css/main.67fedba7.chunk.css"
+  },
+  {
+    "revision": "d9d2c00dc7dc3daea4a2",
+    "url": "/static/js/2.1620ba49.chunk.js"
+  },
+  {
+    "revision": "92de5436519d10d3c7a3",
+    "url": "/static/js/main.ba09b973.chunk.js"
+  },
+  {
+    "revision": "448be9a63c0b9b3d4c09",
+    "url": "/static/js/runtime~main.69e5f513.js"
+  }
+]);
